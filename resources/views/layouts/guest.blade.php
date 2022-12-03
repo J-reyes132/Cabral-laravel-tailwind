06efd2670404ecd14b4c3910de8e5fb73790e44a
j-reyes132 <juanreyes1321@gmail.com>
2022-12-02 20:19:42 -0400

    Fix: design

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -40,8 +40,10 @@
                     class="flex-col mt-8 space-y-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0">
                     <a class="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 hover:text-green-400"
                         href="/">Home</a>
+                        <a class="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 hover:text-green-400"
+                        href="{{ route('login') }}">login</a>
                     <a class="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 hover:text-green-400"
-                        href="{{ route('categories.index') }}">Categories</a>
+                        href="{{ route('categories.index') }}">Combos</a>
                     <a class="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 hover:text-green-400"
                         href="{{ route('menus.index') }}">Our Menu</a>
                     <a class="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 hover:text-green-400"
